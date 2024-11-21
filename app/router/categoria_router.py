b9bc467e700f1a423559a30b69eefb6fd07dbba0
maxtbrsk <135358983+maxtbrsk@users.noreply.github.com>
2024-11-21 18:50:59 -0300

    feat: update categoria router to use type hints for categorias; modify HTML form to correctly handle checkbox input; remove unused SVG and PDF files

--- app/router/categoria_router.py
+++ app/router/categoria_router.py
@@ -1,3 +1,4 @@
+from typing import List
 from fastapi import APIRouter, Form, HTTPException, Request
 from fastapi.responses import HTMLResponse
 from app.controllers.categoria_controller import categories_page
@@ -10,12 +11,12 @@ router = APIRouter()
 
 # Rota pra relacionar um usuário a uma categoria existente
 @router.post("/categorias")
-async def add_prest_catg_route(request: Request, categorias: list = Form(..., alias="categorias[]")):
+async def add_prest_catg_route(request: Request, categorias: List[int] = Form(...)):
     user_id = get_current_user_id(request)
     for categoria in categorias:
         relate_user_to_categoria(prestador_id=user_id, categoria_id=categoria)
     return add_prest_catg(request, categorias)
 
 @router.get("/categoria", response_class=HTMLResponse)
-async def categories_page_route(request: Request, id:int):
+async def categories_page_route(request: Request, id: int):
     return categories_page(request, id)
